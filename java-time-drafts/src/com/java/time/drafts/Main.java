ed85f5155594d2a7bd4db96a68c8b8fa3297a437
Neemias Santos <neemiassgc@outlook.com>
2022-03-11 22:54:29 +0000

    Formatting with SimpleDateFormat

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,21 @@
 package com.java.time.drafts;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tokyo"));
+        Locale.setDefault(Locale.US);
+
+	    final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EEEE dd/MM/yyyy HH:mm:ss");
+        final Calendar calendar = Calendar.getInstance();
+        System.out.println(simpleDateFormat.format(calendar.getTime()));
+
+        final SimpleDateFormat iso8601DateFormatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+        System.out.println(iso8601DateFormatter.format(calendar.getTime()));
     }
 }
