04f71833c2da1467b32f62d5f0c1d44af2cd13eb
Neemias Santos <neemiassgc@outlook.com>
2022-03-14 16:15:20 +0100

    parsing dates using offsets

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,19 @@
 package com.java.time.drafts;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws ParseException {
+	    SimpleDateFormat iso8601Formatter = new SimpleDateFormat("yyyy-MM-dd'T'HH:mmXXX");
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm XXX");
+        formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+        String iso8601Date = "2018-02-01T17:30Z";
+        Date parsedDate = iso8601Formatter.parse(iso8601Date);
+        System.out.println(formatter.format(parsedDate));
     }
 }
