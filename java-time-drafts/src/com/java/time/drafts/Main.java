5cc2650a95d503f2674fa705bf9febe76da41a36
Neemias Santos <neemiassgc@outlook.com>
2022-03-17 22:13:24 +0100

    Some use cases involving dates

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,27 @@
 package com.java.time.drafts;
 
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final TimeZone tz = TimeZone.getTimeZone("America/Los_Angeles");
+        final Calendar cal = Calendar.getInstance(tz, Locale.US);
+
+        System.out.println("Timezone id: "+tz.getID());
+        System.out.println("Timezone name: "+tz.getDisplayName(Locale.US));
+        System.out.println("Timezone offset: "+ TimeUnit.HOURS.convert(tz.getOffset(cal.getTimeInMillis()), TimeUnit.MILLISECONDS));
+        System.out.println("Raw offset: "+ TimeUnit.HOURS.convert(tz.getRawOffset(), TimeUnit.MILLISECONDS));
+        System.out.println("is it daylight savings time? "+tz.inDaylightTime(cal.getTime()));
+
+        final long twoHoursInMillis = TimeUnit.HOURS.toMillis(2);
+        Stream.of(TimeZone.getAvailableIDs()).forEach(id -> {
+            final long offset = TimeZone.getTimeZone(id).getOffset(cal.getTimeInMillis());
+            if (offset == twoHoursInMillis) System.out.println(id);
+        });
     }
 }
