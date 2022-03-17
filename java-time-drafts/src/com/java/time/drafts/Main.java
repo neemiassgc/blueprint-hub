e1ce278be072c466a07fca744c8bf15126b2de42
Neemias Santos <neemiassgc@outlook.com>
2022-03-17 17:51:32 +0100

    Summing hours to a date using the Calendar class

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,32 @@
 package com.java.time.drafts;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        SimpleDateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(java.util.TimeZone.getTimeZone("UTC"));
+        calendar.set(Calendar.YEAR, 2018);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 14);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        // print the date in ISO8601 format
+        System.out.println(iso8601.format(calendar.getTime()));
+
+        // sum two hours to the date
+        calendar.add(Calendar.HOUR, 2);
+        System.out.println(iso8601.format(calendar.getTime()));
     }
 }
