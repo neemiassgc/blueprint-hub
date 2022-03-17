ac662155342e529a865df6e1920e28abc406a5a7
Neemias Santos <neemiassgc@outlook.com>
2022-03-17 17:46:43 +0100

    Summing durations to a date using the Date class

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,42 @@
 package com.java.time.drafts;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
+    private Main() {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        SimpleDateFormat iso8601 = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX", Locale.US);
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeZone(java.util.TimeZone.getTimeZone("UTC"));
+        calendar.set(Calendar.YEAR, 2018);
+        calendar.set(Calendar.MONTH, Calendar.JANUARY);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 14);
+        calendar.set(Calendar.MINUTE, 30);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+
+        // print the date in ISO8601 format
+        Date date = calendar.getTime();
+        System.out.println(iso8601.format(date));
+
+        // sum two hours to the date
+        final long twoHours = TimeUnit.HOURS.toMillis(2);
+        date.setTime(date.getTime() + twoHours);
+        
+        System.out.println(iso8601.format(date));
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        new Main();
+
     }
 }
