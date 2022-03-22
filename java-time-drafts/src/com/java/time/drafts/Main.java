b778d0f0891510eb3f61fc96c9d1ce509fc6052d
Neemias Santos <neemiassgc@outlook.com>
2022-03-22 13:20:44 +0100

    Comparisons with ZonedDateTime

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,20 @@
 package com.java.time.drafts;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ZonedDateTime parisZonedDateTime = ZonedDateTime.of(2019, 10, 15, 0, 0, 0, 0, ZoneId.of("Europe/Paris"));
+        ZonedDateTime tokyoZonedDateTime = ZonedDateTime.of(2019, 10, 15, 7, 0, 0, 0, ZoneId.of("Asia/Tokyo"));
+
+        System.out.println(parisZonedDateTime);
+        System.out.println(tokyoZonedDateTime);
+        System.out.println("parisZonedDateTime equals to tokyoZonedDateTime: "+parisZonedDateTime.equals(tokyoZonedDateTime));
+        System.out.println("parisZonedDateTime is equal to tokyoZonedDateTime: "+parisZonedDateTime.isEqual(tokyoZonedDateTime));
+
+        System.out.println("parisZonedDateTime is before tokyoZonedDateTime: "+parisZonedDateTime.isBefore(tokyoZonedDateTime));
+        System.out.println("parisZonedDateTime is after tokyoZonedDateTime: "+parisZonedDateTime.isAfter(tokyoZonedDateTime));
     }
 }
