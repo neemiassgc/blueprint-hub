2442b598bf4356af68b1c9870da6eb7435123759
Neemias Santos <neemiassgc@outlook.com>
2022-03-22 15:35:50 +0100

    Timezone change to the same instant with ZonedDateTime

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,26 @@
 package com.java.time.drafts;
 
+import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ZonedDateTime zonedDateTime = ZonedDateTime.now(ZoneId.of("America/Sao_Paulo"))
+                .withDayOfMonth(18)
+                .withMonth(Month.FEBRUARY.getValue())
+                .withYear(2018)
+                .withHour(10)
+                .withMinute(2)
+                .withSecond(0);
+
+        System.out.println("Timezone for Sao Paulo");
+        System.out.println(zonedDateTime);
+
+        zonedDateTime = zonedDateTime.withZoneSameInstant(ZoneId.of("Asia/Tokyo"));
+
+        System.out.println("Timezone for Tokyo");
+        System.out.println(zonedDateTime);
     }
 }
