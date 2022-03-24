aa2d091c96405859a2efab76e16e42f1e7a8fed5
Neemias Santos <neemiassgc@outlook.com>
2022-03-24 16:10:59 +0100

    Introduction to OffsetDateTime

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,18 @@
 package com.java.time.drafts;
 
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        OffsetDateTime odt = OffsetDateTime.now();
+
+        ZoneId.getAvailableZoneIds().forEach(zonedId -> {
+            ZoneOffset offset = odt.atZoneSameInstant(ZoneId.of(zonedId)).getOffset();
+
+            if (odt.getOffset().equals(offset)) System.out.println(zonedId+" : "+offset);
+        });
     }
 }
