97a79e5a7834f6a8704cac2735686add4c9f91b2
Neemias Santos <neemiassgc@outlook.com>
2022-03-24 20:27:40 +0100

    Arithmetic with java.time's API

--- java-time-drafts/src/com/java/time/drafts/Main.java
+++ java-time-drafts/src/com/java/time/drafts/Main.java
@@ -1,8 +1,25 @@
 package com.java.time.drafts;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ZonedDateTime zonedDateTime = ZonedDateTime.of(2017, 10, 14, 11, 0, 0, 0, ZoneId.of("America/Sao_Paulo"));
+        System.out.println("original: "+zonedDateTime);
+
+        zonedDateTime = zonedDateTime.plusHours(24);
+        System.out.println("After 24 hours: "+zonedDateTime);
+
+        zonedDateTime = zonedDateTime.minusHours(24);
+        System.out.println("Minus 24 hours: "+zonedDateTime);
+
+        zonedDateTime = zonedDateTime.plusDays(1);
+        System.out.println("After 1 day: "+zonedDateTime);
+
+        zonedDateTime = zonedDateTime.plus(100, ChronoUnit.WEEKS);
+        System.out.println("After 100 weeks: "+zonedDateTime);
     }
 }
